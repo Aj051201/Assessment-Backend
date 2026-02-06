98d74a8c0055d4a2ecf6bdc3ac8a98c98c6a3299
Aj051201 <ajayverma0138@gmail.com>
2026-02-06 17:49:36 +0530

    update initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    origins 'http://localhost:5173', 'https://assessment-frontend-1.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options]
