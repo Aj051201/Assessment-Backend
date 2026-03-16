b84ef1631734d8a54d3057e9aa07f988b9722f9a
Aj051201 <ajayverma0138@gmail.com>
2026-03-16 12:07:39 +0530

    Add user model dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :publications
+  has_many :publications, dependent: :destroy
   validates :email, presence: true, uniqueness: true
 end
